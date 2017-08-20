6fb03dac31a195a59d3f56a53a21ee10eb4bad0a
johnbrown <john.brown@yoti.com>
2017-08-20 18:44:20 +0100

    updated largest prime factor

--- 3_largest_prime_factor.py
+++ 3_largest_prime_factor.py
@@ -11,26 +11,38 @@ What is the largest prime factor of the number 600851475143 ?
 def checkPrime(num):
     if num == 0 or num == 1:
         return 0
-    for x in range(2, num):
-        if num % x == 0:
-            return 0
+    if num % 2 == 0:
+        return 0
+    elif num % 3 == 0:
+        return 0
+    elif num % 5 == 0:
+        return 0
+    elif num % 7 == 0:
+        return 0
     else:
         return 1
 
 def genPrime(top):
     prime = []
-    for x in range((top + 1), 1, -1):
+    for x in range((top + 1), (top - 1000000), -1):
         if checkPrime(x):
-            print(x)
             prime.append(x)
     return(prime)
 
-target = 13195
+target = 600851475143
 prime = genPrime(target)
 
+print(prime[0])
+
+"""
 for x in prime:
     if target % x:
         print(x)
     break
 
-
+primes = []
+target = 600851475143
+for x in range(1, target):
+    print(x)
+    primes.append(x)
+"""
