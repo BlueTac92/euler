f404b3041522763c0022c9d2024d110303e43be0
johnbrown <BlueTac92@gmail.com>
2018-05-30 21:58:58 +0100

    added new version for challenge 3, much cleaner and easier to read

--- 3_largest_prime_factor_2.py
+++ 3_largest_prime_factor_2.py
@@ -1,39 +1,23 @@
 #!/usr/bin/python
 
 """
-
 The prime factors of 13195 are 5, 7, 13 and 29.
 
 What is the largest prime factor of the number 600851475143 ?
-
 """
 
 from math import sqrt
 
-def checkPrime(num):
-    if num == 0 or num == 1:
-        return 0
-    elif num % 2 == 0:
-        return 0
-    elif num % 3 == 0:
-        return 0
-    elif num % 5 == 0:
-        return 0
-    elif num % 7 == 0:
-        return 0
-    else:
-      #  print(str(num) + " is a prime")
-        return 1
-
-def genPrime(top):
-    prime = []
-    for x in range(2, square):
-        if target % x == 0:
-            if checkPrime(x):
-                print(x)
-                prime.append(x)
-    return(prime)
-
 target = 600851475143
-square = int(sqrt(target))
-prime = genPrime(square)
+high_prime = 0
+
+fib = [x for x in range(2, int(sqrt(target))) if target % x == 0]
+
+for fib in fib:
+    for x in range(2,fib-1):
+        if fib % x == 0:
+            if x > high_prime:
+                high_prime = x
+
+print(high_prime)
+
